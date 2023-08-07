4d735c4ce87452d8c0689e5797e3eb9c0ca4ff37
sauransh <82646949+sauransh@users.noreply.github.com>
2023-08-07 15:47:37 +0000

    Stack implemented with list

---
+++ Stacks interview Questions/Stack_implementation_with_LIST.py
@@ -0,0 +1,33 @@
+class Stack:
+    def __init__(self):
+        self.stack_list = []
+
+    def print_stack_list(self):
+        for i in range(len(self.stack_list)-1,-1,-1):
+            print(self.stack_list[i])
+    
+    def is_empty(self):
+        return len(self.stack_list) == 0 # returns True/False
+    
+    def peek(self):
+        if self.is_empty():
+            return None
+        else:
+            return self.stack_list[-1]
+    
+    def size(self):
+        return len(self.stack_list)
+    
+    def push(self,value):
+        self.stack_list.append(value)
+
+    def pop(self):
+        if self.is_empty():
+            return None
+        else:
+            return self.stack_list.pop()
+
+    
+        
+
+    
