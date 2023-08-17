74d4ee1720f893e90a2ed1db315fe205e7baac06
sauransh <82646949+sauransh@users.noreply.github.com>
2023-08-17 18:04:43 +0000

    longest seq ****

--- HashTable interview questions/set.py
+++ HashTable interview questions/set.py
@@ -65,3 +65,20 @@ print (find_pairs(arr1, arr2, target))
 
 
 #print(has_unique_char('abacdef'))
+
+def longest_consecutive_sequence(nums):
+    num_set = set(nums)
+    longest_sequence = 0
+    
+    for num in nums:
+        if num - 1 not in num_set:
+            current_num = num
+            current_sequence = 1
+            
+            while current_num + 1 in num_set:
+                current_num += 1
+                current_sequence += 1
+            
+            longest_sequence = max(longest_sequence, current_sequence)
+    
+    return longest_sequence
